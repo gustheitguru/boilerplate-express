8cd97c877184831135409d34c4f10e5f69b82133
Gus <gusr@logicliving.com>
2020-05-17 22:27:43 -0500

    adding .env
    added .env file with varible
    
    had to comment out step 5
    
    wrote and if statment into the function part of app.get() to check the varible in the env file

--- myApp.js
+++ myApp.js
@@ -43,9 +43,9 @@ app.use("/assets", express.static(__dirname + "/public"));
 // this is for CSS, images file and JS Scripts
 
 /** 5) serve JSON on a specific route */
-app.get('/json', (req, res) => {
-  res.json({"message": "Hello json"});
-});
+// app.get('/json', (req, res) => {
+//   res.json({"message": "Hello json"});
+// });
 //JSON response test by going to url/json
 // put in /json as directory of json data 
 // can run fucntions to return real time data
@@ -53,7 +53,15 @@ app.get('/json', (req, res) => {
 
 
 /** 6) Use the .env file to configure the app */
- 
+// commands are processed as writen. first to last had to comment out step 5 to get step 6 to work
+app.get('/json', (req, res) => {
+if (process.env.MESSAGE_STYLE === "uppercase")
+  res.json({"message": "HELLO JSON"});
+}) else
+  res.json({"message": "Hello json"});
+// process.env looks at the .env file and checks for varibles
+// if statment to check which response to send 
+
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
